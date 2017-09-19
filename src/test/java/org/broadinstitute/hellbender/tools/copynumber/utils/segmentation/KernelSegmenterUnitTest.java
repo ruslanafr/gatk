7d4eca1d0bc4e36d232b0b9f527c32559e8a106f
Samuel Lee <slee@broadinstitute.org>
2017-09-19 13:42:58 -0400

    added kernel seg tests

--- src/test/java/org/broadinstitute/hellbender/tools/copynumber/utils/segmentation/KernelSegmenterUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/copynumber/utils/segmentation/KernelSegmenterUnitTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.function.BiFunction;
@@ -33,8 +34,16 @@ public class KernelSegmenterUnitTest extends BaseTest {
         final List<Integer> changepointsExpectedGaussian = Arrays.asList(
                 299, 699, 99, 899, 399, 199, 599, 499, 799);            //from python implementation
 
+        rng.setSeed(RANDOM_SEED);
+        final List<Double> dataZeroMeanMultimodal = IntStream.range(0, numPoints).boxed()
+                .map(i -> 2 * (rng.nextBoolean() ? -1 : 1) * Math.abs(i / 100 - 5) + 0.1 * rng.nextGaussian())
+                .collect(Collectors.toList());
+        final List<Integer> changepointsExpectedZeroMeanMultimodal = Arrays.asList(
+                499, 599, 799, 699, 399, 899, 299, 199, 99);            //from python implementation
+
         return new Object[][]{
-                {dataGaussian, linearKernel, changepointsExpectedGaussian}
+                {dataGaussian, linearKernel, changepointsExpectedGaussian},
+                {dataZeroMeanMultimodal, gaussianKernel, changepointsExpectedZeroMeanMultimodal}
         };
     }
 
@@ -56,5 +65,58 @@ public class KernelSegmenterUnitTest extends BaseTest {
                         numChangepointsPenaltyLinearFactor, numChangepointsPenaltyLogLinearFactor, true);
         Assert.assertEquals(changepoints, changepointsExpected);
         Assert.assertEquals(changepointsIndexSorted, changepointsExpected.stream().sorted().collect(Collectors.toList()));
+    }
+
+    @Test(dataProvider = "dataKernelSegmenter")
+    public void testKernelSegmenterTruncateChangepoints(final List<Double> data,
+                                                        final BiFunction<Double, Double, Double> kernel,
+                                                        final List<Integer> changepointsExpected) {
+        final int maxNumChangepoints = 5;
+        final int kernelApproximationDimension = 20;
+        final List<Integer> windowSizes = Arrays.asList(8, 16, 32, 64);
+        final double numChangepointsPenaltyLinearFactor = 2.;
+        final double numChangepointsPenaltyLogLinearFactor = 2.;
+
+        final List<Integer> changepoints = new KernelSegmenter<>(data)
+                .findChangepoints(maxNumChangepoints, kernel, kernelApproximationDimension, windowSizes,
+                        numChangepointsPenaltyLinearFactor, numChangepointsPenaltyLogLinearFactor, false);
+        Assert.assertEquals(changepoints, changepointsExpected.subList(0, maxNumChangepoints));
+    }
+
+    @Test(dataProvider = "dataKernelSegmenter")
+    public void testKernelSegmenterExtremePenalty(final List<Double> data,
+                                    final BiFunction<Double, Double, Double> kernel,
+                                    final List<Integer> changepointsExpected) {
+        final int maxNumChangepoints = 25;
+        final int kernelApproximationDimension = 20;
+        final List<Integer> windowSizes = Arrays.asList(8, 16, 32, 64);
+        final double numChangepointsPenaltyLinearFactor = 10000.;
+        final double numChangepointsPenaltyLogLinearFactor = 10000.;
+
+        final List<Integer> changepoints = new KernelSegmenter<>(data)
+                .findChangepoints(maxNumChangepoints, kernel, kernelApproximationDimension, windowSizes,
+                        numChangepointsPenaltyLinearFactor, numChangepointsPenaltyLogLinearFactor, false);
+        final List<Integer> changepointsIndexSorted = new KernelSegmenter<>(data)
+                .findChangepoints(maxNumChangepoints, kernel, kernelApproximationDimension, windowSizes,
+                        numChangepointsPenaltyLinearFactor, numChangepointsPenaltyLogLinearFactor, true);
+        Assert.assertEquals(changepoints, Collections.emptyList());
+        Assert.assertEquals(changepointsIndexSorted, Collections.emptyList());
+    }
+
+    @Test(dataProvider = "dataKernelSegmenter")
+    public void testKernelSegmenterNoPenalty(final List<Double> data,
+                                             final BiFunction<Double, Double, Double> kernel,
+                                             final List<Integer> changepointsExpected) {
+        final int maxNumChangepoints = 25;
+        final int kernelApproximationDimension = 20;
+        final List<Integer> windowSizes = Arrays.asList(8, 16, 32, 64);
+        final double numChangepointsPenaltyLinearFactor = 0.;
+        final double numChangepointsPenaltyLogLinearFactor = 0.;
+
+        final List<Integer> changepoints = new KernelSegmenter<>(data)
+                .findChangepoints(maxNumChangepoints, kernel, kernelApproximationDimension, windowSizes,
+                        numChangepointsPenaltyLinearFactor, numChangepointsPenaltyLogLinearFactor, false);
+        Assert.assertEquals(changepoints.size(), maxNumChangepoints);
+        Assert.assertEquals(changepoints.subList(0, changepointsExpected.size()), changepointsExpected);
     }
 }

5d5c20eb8acf5bd1dcf8794975f4865bfbd328af
Samuel Lee <lee.samuel.k@gmail.com>
2017-09-21 14:59:33 -0400

    data provider for DenoiseReadCountsIntegrationTest

--- src/test/java/org/broadinstitute/hellbender/tools/copynumber/legacy/DenoiseReadCountsIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/copynumber/legacy/DenoiseReadCountsIntegrationTest.java
@@ -1,19 +1,18 @@
 package org.broadinstitute.hellbender.tools.copynumber.legacy;
 
 import org.broadinstitute.hellbender.CommandLineProgramTest;
-import org.broadinstitute.hellbender.cmdline.ExomeStandardArgumentDefinitions;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
-import org.broadinstitute.hellbender.tools.copynumber.legacy.coverage.copyratio.CopyRatio;
 import org.broadinstitute.hellbender.tools.copynumber.legacy.coverage.copyratio.CopyRatioCollection;
 import org.broadinstitute.hellbender.tools.copynumber.legacy.formats.LegacyCopyNumberArgument;
-import org.broadinstitute.hellbender.tools.exome.TargetArgumentCollection;
+import org.broadinstitute.hellbender.utils.test.ArgumentsBuilder;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
-import java.util.stream.Collectors;
-
-import static org.testng.Assert.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Integration tests for {@link DenoiseReadCounts}.
@@ -28,16 +27,39 @@ public class DenoiseReadCountsIntegrationTest extends CommandLineProgramTest {
     private static final File WGS_NO_GC_PON_FILE = new File(largeFileTestDir, "cnv_somatic_workflows_test_files/wgs-no-gc.pon.hdf5");
     private static final File WGS_DO_GC_PON_FILE = new File(largeFileTestDir, "cnv_somatic_workflows_test_files/wgs-do-gc.pon.hdf5");
 
-    @Test
-    public void testWithNoGCPonWithoutAnnotations() {
+    //create all combinations of input
+    @DataProvider(name = "dataDenoiseReadCounts")
+    public Object[][] dataDenoiseReadCounts() {
+
+        final List<List<Object>> data = new ArrayList<>();
+        for (final File inputReadCountsFile : Arrays.asList(WGS_READ_COUNTS_TSV_FILE, WGS_READ_COUNTS_HDF5_FILE)) {
+            for (final File annotatedIntervalsFile : Arrays.asList(WGS_ANNOTATED_INTERVALS_FILE, null)) {
+                for (final File ponFile : Arrays.asList(WGS_NO_GC_PON_FILE, WGS_DO_GC_PON_FILE, null)) {
+                    final ArgumentsBuilder arguments = new ArgumentsBuilder()
+                            .addFileArgument(StandardArgumentDefinitions.INPUT_SHORT_NAME, inputReadCountsFile);
+                    if (annotatedIntervalsFile != null) {
+                        arguments.addFileArgument(LegacyCopyNumberArgument.ANNOTATED_INTERVALS_FILE_SHORT_NAME, annotatedIntervalsFile);
+                    }
+                    if (ponFile != null) {
+                        arguments.addFileArgument(LegacyCopyNumberArgument.READ_COUNT_PANEL_OF_NORMALS_FILE_SHORT_NAME, ponFile);
+                    }
+                    data.add(Arrays.asList(arguments, ponFile != null));
+                }
+            }
+        }
+        return data.stream().map(List::toArray).toArray(Object[][]::new);
+    }
+
+    @Test(dataProvider = "dataDenoiseReadCounts")
+    public void testDenoiseReadCounts(final ArgumentsBuilder argumentsBuilder,
+                                      final boolean isWithPoN) {
         final File standardizedCRFile = createTempFile("test", ".standardizedCR.tsv");
         final File denoisedCRFile = createTempFile("test", ".denoisedCR.tsv");
-        final String[] arguments = {
-                "-" + StandardArgumentDefinitions.INPUT_SHORT_NAME, WGS_READ_COUNTS_TSV_FILE.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.READ_COUNT_PANEL_OF_NORMALS_FILE_SHORT_NAME, WGS_NO_GC_PON_FILE.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.STANDARDIZED_COPY_RATIOS_FILE_SHORT_NAME, standardizedCRFile.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.DENOISED_COPY_RATIOS_FILE_SHORT_NAME, denoisedCRFile.getAbsolutePath()
-        };
+        final String[] arguments = argumentsBuilder
+                .addFileArgument(LegacyCopyNumberArgument.STANDARDIZED_COPY_RATIOS_FILE_SHORT_NAME, standardizedCRFile)
+                .addFileArgument(LegacyCopyNumberArgument.DENOISED_COPY_RATIOS_FILE_SHORT_NAME, denoisedCRFile)
+                .addArgument(StandardArgumentDefinitions.VERBOSITY_NAME, "INFO")
+                .getArgsArray();
         runCommandLine(arguments);
 
         Assert.assertTrue(standardizedCRFile.exists());
@@ -46,186 +68,10 @@ public class DenoiseReadCountsIntegrationTest extends CommandLineProgramTest {
         final CopyRatioCollection standardizedCopyRatios = new CopyRatioCollection(standardizedCRFile);
         final CopyRatioCollection denoisedCopyRatios = new CopyRatioCollection(denoisedCRFile);
         Assert.assertFalse(standardizedCopyRatios == denoisedCopyRatios);
-        //sample names and intervals should be the same, standardized and denoised copy ratios should be different
-        //we do not check correctness of the output standardized or denoised copy ratios
+        //sample names and intervals should always be the same
         Assert.assertEquals(standardizedCopyRatios.getSampleName(), denoisedCopyRatios.getSampleName());
         Assert.assertEquals(standardizedCopyRatios.getIntervals(), denoisedCopyRatios.getIntervals());
-        Assert.assertNotEquals(standardizedCopyRatios.getCopyRatioValues(), denoisedCopyRatios.getCopyRatioValues());
-    }
-
-    @Test
-    public void testHDF5InputWithNoGCPonWithoutAnnotations() {
-        final File standardizedCRFile = createTempFile("test", ".standardizedCR.tsv");
-        final File denoisedCRFile = createTempFile("test", ".denoisedCR.tsv");
-        final String[] arguments = {
-                "-" + StandardArgumentDefinitions.INPUT_SHORT_NAME, WGS_READ_COUNTS_HDF5_FILE.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.READ_COUNT_PANEL_OF_NORMALS_FILE_SHORT_NAME, WGS_NO_GC_PON_FILE.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.STANDARDIZED_COPY_RATIOS_FILE_SHORT_NAME, standardizedCRFile.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.DENOISED_COPY_RATIOS_FILE_SHORT_NAME, denoisedCRFile.getAbsolutePath()
-        };
-        runCommandLine(arguments);
-
-        Assert.assertTrue(standardizedCRFile.exists());
-        Assert.assertTrue(denoisedCRFile.exists());
-
-        final CopyRatioCollection standardizedCopyRatios = new CopyRatioCollection(standardizedCRFile);
-        final CopyRatioCollection denoisedCopyRatios = new CopyRatioCollection(denoisedCRFile);
-        Assert.assertFalse(standardizedCopyRatios == denoisedCopyRatios);
-        //sample names and intervals should be the same, standardized and denoised copy ratios should be different
-        //we do not check correctness of the output standardized or denoised copy ratios
-        Assert.assertEquals(standardizedCopyRatios.getSampleName(), denoisedCopyRatios.getSampleName());
-        Assert.assertEquals(standardizedCopyRatios.getIntervals(), denoisedCopyRatios.getIntervals());
-        Assert.assertNotEquals(standardizedCopyRatios.getCopyRatioValues(), denoisedCopyRatios.getCopyRatioValues());
-    }
-
-    @Test
-    public void testWithNoGCPonWithAnnotations() {
-        final File standardizedCRFile = createTempFile("test", ".standardizedCR.tsv");
-        final File denoisedCRFile = createTempFile("test", ".denoisedCR.tsv");
-        final String[] arguments = {
-                "-" + StandardArgumentDefinitions.INPUT_SHORT_NAME, WGS_READ_COUNTS_TSV_FILE.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.READ_COUNT_PANEL_OF_NORMALS_FILE_SHORT_NAME, WGS_NO_GC_PON_FILE.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.ANNOTATED_INTERVALS_FILE_SHORT_NAME, WGS_ANNOTATED_INTERVALS_FILE.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.STANDARDIZED_COPY_RATIOS_FILE_SHORT_NAME, standardizedCRFile.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.DENOISED_COPY_RATIOS_FILE_SHORT_NAME, denoisedCRFile.getAbsolutePath()
-        };
-        runCommandLine(arguments);
-
-        Assert.assertTrue(standardizedCRFile.exists());
-        Assert.assertTrue(denoisedCRFile.exists());
-
-        final CopyRatioCollection standardizedCopyRatios = new CopyRatioCollection(standardizedCRFile);
-        final CopyRatioCollection denoisedCopyRatios = new CopyRatioCollection(denoisedCRFile);
-        Assert.assertFalse(standardizedCopyRatios == denoisedCopyRatios);
-        //sample names and intervals should be the same, standardized and denoised copy ratios should be different
-        //we do not check correctness of the output standardized or denoised copy ratios
-        Assert.assertEquals(standardizedCopyRatios.getSampleName(), denoisedCopyRatios.getSampleName());
-        Assert.assertEquals(standardizedCopyRatios.getIntervals(), denoisedCopyRatios.getIntervals());
-        Assert.assertNotEquals(standardizedCopyRatios.getCopyRatioValues(), denoisedCopyRatios.getCopyRatioValues());
-    }
-
-    @Test
-    public void testWithoutPoNWithAnnotations() {
-        final File standardizedCRFile = createTempFile("test", ".standardizedCR.tsv");
-        final File denoisedCRFile = createTempFile("test", ".denoisedCR.tsv");
-        final String[] arguments = {
-                "-" + StandardArgumentDefinitions.INPUT_SHORT_NAME, WGS_READ_COUNTS_HDF5_FILE.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.ANNOTATED_INTERVALS_FILE_SHORT_NAME, WGS_ANNOTATED_INTERVALS_FILE.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.STANDARDIZED_COPY_RATIOS_FILE_SHORT_NAME, standardizedCRFile.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.DENOISED_COPY_RATIOS_FILE_SHORT_NAME, denoisedCRFile.getAbsolutePath()
-        };
-        runCommandLine(arguments);
-
-        Assert.assertTrue(standardizedCRFile.exists());
-        Assert.assertTrue(denoisedCRFile.exists());
-
-        final CopyRatioCollection standardizedCopyRatios = new CopyRatioCollection(standardizedCRFile);
-        final CopyRatioCollection denoisedCopyRatios = new CopyRatioCollection(denoisedCRFile);
-        Assert.assertFalse(standardizedCopyRatios == denoisedCopyRatios);
-        //sample names, intervals, and standardized and denoised copy ratios should be the same when no PoN is provided
-        //we do not check correctness of the output standardized or denoised copy ratios
-        Assert.assertEquals(standardizedCopyRatios, denoisedCopyRatios);
-    }
-
-    @Test
-    public void testWithoutPoNWithoutAnnotations() {
-        final File standardizedCRFile = createTempFile("test", ".standardizedCR.tsv");
-        final File denoisedCRFile = createTempFile("test", ".denoisedCR.tsv");
-        final String[] arguments = {
-                "-" + StandardArgumentDefinitions.INPUT_SHORT_NAME, WGS_READ_COUNTS_HDF5_FILE.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.STANDARDIZED_COPY_RATIOS_FILE_SHORT_NAME, standardizedCRFile.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.DENOISED_COPY_RATIOS_FILE_SHORT_NAME, denoisedCRFile.getAbsolutePath()
-        };
-        runCommandLine(arguments);
-
-        Assert.assertTrue(standardizedCRFile.exists());
-        Assert.assertTrue(denoisedCRFile.exists());
-
-        final CopyRatioCollection standardizedCopyRatios = new CopyRatioCollection(standardizedCRFile);
-        final CopyRatioCollection denoisedCopyRatios = new CopyRatioCollection(denoisedCRFile);
-        Assert.assertFalse(standardizedCopyRatios == denoisedCopyRatios);
-        //sample names, intervals, and standardized and denoised copy ratios should be the same when no PoN is provided
-        //we do not check correctness of the output standardized or denoised copy ratios
-        Assert.assertEquals(standardizedCopyRatios, denoisedCopyRatios);
-    }
-
-    @Test
-    public void testWithDoGCPonWithoutAnnotations() {
-        final File standardizedCRFile = createTempFile("test", ".standardizedCR.tsv");
-        final File denoisedCRFile = createTempFile("test", ".denoisedCR.tsv");
-        final String[] arguments = {
-                "-" + StandardArgumentDefinitions.INPUT_SHORT_NAME, WGS_READ_COUNTS_TSV_FILE.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.READ_COUNT_PANEL_OF_NORMALS_FILE_SHORT_NAME, WGS_DO_GC_PON_FILE.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.STANDARDIZED_COPY_RATIOS_FILE_SHORT_NAME, standardizedCRFile.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.DENOISED_COPY_RATIOS_FILE_SHORT_NAME, denoisedCRFile.getAbsolutePath()
-        };
-        runCommandLine(arguments);
-
-        Assert.assertTrue(standardizedCRFile.exists());
-        Assert.assertTrue(denoisedCRFile.exists());
-
-        final CopyRatioCollection standardizedCopyRatios = new CopyRatioCollection(standardizedCRFile);
-        final CopyRatioCollection denoisedCopyRatios = new CopyRatioCollection(denoisedCRFile);
-        Assert.assertFalse(standardizedCopyRatios == denoisedCopyRatios);
-        //sample names and intervals should be the same, standardized and denoised copy ratios should be different
-        //we do not check correctness of the output standardized or denoised copy ratios
-        Assert.assertEquals(standardizedCopyRatios.getSampleName(), denoisedCopyRatios.getSampleName());
-        Assert.assertEquals(standardizedCopyRatios.getIntervals(), denoisedCopyRatios.getIntervals());
-        Assert.assertNotEquals(standardizedCopyRatios.getCopyRatioValues(), denoisedCopyRatios.getCopyRatioValues());
-    }
-
-    @Test
-    public void testHDF5InputWithDoGCPonWithoutAnnotations() {
-        final File standardizedCRFile = createTempFile("test", ".standardizedCR.tsv");
-        final File denoisedCRFile = createTempFile("test", ".denoisedCR.tsv");
-        final String[] arguments = {
-                "-" + StandardArgumentDefinitions.INPUT_SHORT_NAME, WGS_READ_COUNTS_HDF5_FILE.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.READ_COUNT_PANEL_OF_NORMALS_FILE_SHORT_NAME, WGS_NO_GC_PON_FILE.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.STANDARDIZED_COPY_RATIOS_FILE_SHORT_NAME, standardizedCRFile.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.DENOISED_COPY_RATIOS_FILE_SHORT_NAME, denoisedCRFile.getAbsolutePath()
-        };
-        runCommandLine(arguments);
-
-        Assert.assertTrue(standardizedCRFile.exists());
-        Assert.assertTrue(denoisedCRFile.exists());
-
-        final CopyRatioCollection standardizedCopyRatios = new CopyRatioCollection(standardizedCRFile);
-        final CopyRatioCollection denoisedCopyRatios = new CopyRatioCollection(denoisedCRFile);
-        Assert.assertFalse(standardizedCopyRatios == denoisedCopyRatios);
-        //sample names and intervals should be the same, standardized and denoised copy ratios should be different
-        //we do not check correctness of the output standardized or denoised copy ratios
-        Assert.assertEquals(standardizedCopyRatios.getSampleName(), denoisedCopyRatios.getSampleName());
-        Assert.assertEquals(standardizedCopyRatios.getIntervals(), denoisedCopyRatios.getIntervals());
-        Assert.assertNotEquals(standardizedCopyRatios.getCopyRatioValues(), denoisedCopyRatios.getCopyRatioValues());
-    }
-
-    @Test
-    public void testWithDoGCPonWithAnnotations() {
-        final File standardizedCRFile = createTempFile("test", ".standardizedCR.tsv");
-        final File denoisedCRFile = createTempFile("test", ".denoisedCR.tsv");
-        final String[] arguments = {
-                "-" + StandardArgumentDefinitions.INPUT_SHORT_NAME, WGS_READ_COUNTS_TSV_FILE.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.READ_COUNT_PANEL_OF_NORMALS_FILE_SHORT_NAME, WGS_DO_GC_PON_FILE.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.ANNOTATED_INTERVALS_FILE_SHORT_NAME, WGS_ANNOTATED_INTERVALS_FILE.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.STANDARDIZED_COPY_RATIOS_FILE_SHORT_NAME, standardizedCRFile.getAbsolutePath(),
-                "-" + LegacyCopyNumberArgument.DENOISED_COPY_RATIOS_FILE_SHORT_NAME, denoisedCRFile.getAbsolutePath(),
-                "-" + StandardArgumentDefinitions.VERBOSITY_NAME, "INFO"
-        };
-        runCommandLine(arguments);
-
-        //check that warning is thrown when both PoN with GC annotations and GC-annotated intervals are provided
-
-
-        Assert.assertTrue(standardizedCRFile.exists());
-        Assert.assertTrue(denoisedCRFile.exists());
-
-        final CopyRatioCollection standardizedCopyRatios = new CopyRatioCollection(standardizedCRFile);
-        final CopyRatioCollection denoisedCopyRatios = new CopyRatioCollection(denoisedCRFile);
-        Assert.assertFalse(standardizedCopyRatios == denoisedCopyRatios);
-        //sample names and intervals should be the same, standardized and denoised copy ratios should be different
-        //we do not check correctness of the output standardized or denoised copy ratios
-        Assert.assertEquals(standardizedCopyRatios.getSampleName(), denoisedCopyRatios.getSampleName());
-        Assert.assertEquals(standardizedCopyRatios.getIntervals(), denoisedCopyRatios.getIntervals());
-        Assert.assertNotEquals(standardizedCopyRatios.getCopyRatioValues(), denoisedCopyRatios.getCopyRatioValues());
+        //standardized and denoised copy ratios should be the same if PoN is not provided
+        Assert.assertEquals(standardizedCopyRatios.getCopyRatioValues().equals(denoisedCopyRatios.getCopyRatioValues()), !isWithPoN);
     }
 }
